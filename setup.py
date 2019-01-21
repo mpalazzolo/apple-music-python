74ff8c4d9704c9b71fc26c5bdd3e9ca8db29891a
mpalazzolo <mattpalazzolo@gmail.com>
2019-01-21 18:20:20 -0500

    updated version in setup file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 setup(
     name='apple-music-python',
     url='https://github.com/mpalazzolo/apple-music-python',
-    version='1.0.1',
+    version='1.0.2',
     packages=['applemusicpy'],
     license='LICENSE.txt',
     author='Matt Palazzolo',
